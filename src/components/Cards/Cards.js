fa84915d1f5cb41f050cafd7cfacf33d6fc24a47
mkamilmistar <kamil_mistar@apps.ipb.ac.id>
2021-03-19 10:28:05 +0700

    pic

--- src/components/Cards/Cards.js
+++ src/components/Cards/Cards.js
@@ -6,15 +6,18 @@ function Cards() {
         <div className="project">
             <Card 
                 title="Patroli Karhutla"
-                imgURL="gif/karhutla.gif"
-                body="The Karhutla Patrol application is used for the data collection process by an integrated patrol team in the field. In the Karhutla Patrol application, there are features to fill out reports, view reports, and visualize hotspots obtained from the Sipongi application and server owned by KLHK.
+                imgURL="images/karhutla.gif"
+                body="The Karhutla Patrol application is used for the data collection process by an 
+                integrated patrol team in the field. In the Karhutla Patrol application, there are 
+                features to fill out reports, view reports, and visualize hotspots obtained from the Sipongi 
+                application and server owned by Kementerian Lingkungan Hidup dan Kehutanan (KLHK).
                 "
                 small="AngularJs + Ionic 5"
                 link="//github.com/AppKarhutlaNew"
             />
             <Card
                 title="YukZakat"
-                imgURL="gif/yukzakat.gif"
+                imgURL="images/yukzakat.png"
                 body="This web application is equipped with an online zakat payment feature and a zakat calculator. This zakat calculator can show that the congregation's assets have not reached the limit (nishab) and when it has reached the nishab, it will show how much zakat must be paid.
                 "
                 small="Bootstrap (Frontend) + Laravel (Backend)"
@@ -22,7 +25,7 @@ function Cards() {
             />
             <Card 
                 title="Website Desa Pekan Kamis"
-                imgURL="gif/pekankamis.gif"
+                imgURL="images/pekankamis.png"
                 body="This website is a means of publication to provide information and an overview of the potential of Pekan Kamis, Tembilahan Hulu, Indragiri Hilir, Riau villages as well as improving services to the public to access information.
                 "
                 small="Wordpress"
@@ -30,7 +33,7 @@ function Cards() {
             />
             <Card
                 title="SIMAS ASRAMA SCB"
-                imgURL="gif/simas.gif"
+                imgURL="images/simas.png"
                 body="The Management Information System for SMP Cendekia Baznas is the management of dormitory activities, including memorization records, good and bad notes, points of violation by SCB students.
                 "
                 small="Bootstrap (Frontend) + Laravel (Backend)"
